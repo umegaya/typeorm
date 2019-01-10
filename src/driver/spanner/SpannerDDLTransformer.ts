bfc7606a3ebc9dfe5fe28e9c710f0c9538c7b98f
Takehiro Iyatomi <iyatomi@gmail.com>
2019-01-10 09:02:29 +0900

    fix ddl transformer and ddl parser bug

--- src/driver/spanner/SpannerDDLTransformer.ts
+++ src/driver/spanner/SpannerDDLTransformer.ts
@@ -207,7 +207,7 @@ export class SpannerDDLTransformer {
             return "NOT NULL";
         } else if (ast.autoincrement) {
             this.addExtendSchema(extendSchemas, "generator", "increment");
-        } else if (ast.default) {
+        } else if (ast.default !== undefined) {
             this.addExtendSchema(extendSchemas, "default", JSON.stringify(ast.default));
         }
         return "";
